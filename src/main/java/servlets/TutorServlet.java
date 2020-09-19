705b96edec971e3cc1b62ae1c743d8ceb2d845bb
Sarah <szou21136@gmail.com>
2020-09-19 20:52:56 +0000

    TutorServlet and StudentServlet up and running

--- src/main/java/servlets/TutorServlet.java
+++ src/main/java/servlets/TutorServlet.java
@@ -7,45 +7,51 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
+import org.json.JSONObject;  
 
-/** Servlet that returns Tutor data */
+/** Servlet that returns comment data */
 @WebServlet("/tutor")
 public class TutorServlet extends HttpServlet {
-     
+  private final String[] TUTOR_FIELDS = {"name", "meeting-medium", "location", "time-zone", "subjects", "num-students", "tutor-rates", "bio"};
+  
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-     
-    //int maxTutors = getNumDisplayTutors(request);
-     // Retrieves Tutor data from datastore. 
-     // Displays most recent Tutors first.
+
     Query query = new Query("Tutor");
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
-    //create a tutor data class?
-    // Create arraylist of string to store Tutors.
-    ArrayList<String> tutors = new ArrayList<>();
-    for (Entity entity : results.asList(FetchOptions.Builder.withLimit(10))) {
-      String name = (String) entity.getProperty("name");
-      String text = (String) entity.getProperty("text");
-      tutors.add(name + ": " + text + "\n");  
+
+    // Create arraylist of string to store tutors.
+    ArrayList<JSONObject> tutors = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+        String tutorJSON = "{";
+        for(int i = 0; i < TUTOR_FIELDS.length; i++){
+            String field = TUTOR_FIELDS[i];
+            tutorJSON += "\"" + field + "\"" + ":" + (String) entity.getProperty(field);
+            if (i != TUTOR_FIELDS.length - 1){
+                tutorJSON += ",";
+            }
+        }
+        tutorJSON += "}";
+        JSONObject object = new JSONObject(tutorJSON);
+        tutors.add(object);
     }
 
     Gson gson = new Gson();
 
-    response.setContentType("application/json;");
+    response.setContentType("application/json");
     response.getWriter().println(gson.toJson(tutors));
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    //enumeration name and value into datastore
-    String name = request.getParameter("name");
-    String text = request.getParameter("text-input");
-    long timestamp = System.currentTimeMillis();
     Entity TutorEntity = new Entity("Tutor");
-    TutorEntity.setProperty("name", name);
-    TutorEntity.setProperty("text", text);
+    //Enumeration fields = request.getParameterNames();
+    for (String currField: TUTOR_FIELDS){
+        String value = request.getParameter(currField);
+        TutorEntity.setProperty(currField, value);
+    }
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(TutorEntity);
     response.sendRedirect("/index.html");
